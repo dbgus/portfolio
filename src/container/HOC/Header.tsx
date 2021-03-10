37920d971024d178b879db2394a45efa5affba6a
dbgus <dbgus0101@gmail.com>
2021-03-10 22:47:38 +0900

    main-content and header ibase

--- src/container/HOC/Header.tsx
+++ src/container/HOC/Header.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "./_header.scss";
 
 export default function Header() {
-  return <div>asdasd</div>;
+  return (
+    <div className="header">
+      <p>home</p>
+      <p>about</p>
+      <p>connect</p>
+    </div>
+  );
 }
--- src/container/main/Main.tsx
+++ src/container/main/Main.tsx
@@ -5,11 +5,10 @@ export default function Main() {
   return (
     <div className="main">
       <div className="main-box">
+        <div className="overlay" />
         <div className="main-box-content-zone">
           <p className="main-box-content-title">Hi, I'm eddy</p>
-        </div>
-        <div className="main-box-image">
-          <img src={Cat} alt="cat" />
+          <p className="main-box-content-content">web full stack developer</p>
         </div>
       </div>
     </div>
